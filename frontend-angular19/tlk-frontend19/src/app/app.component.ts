ce15f1cce01ac1b85651cdba404bc8cca3b443e7
Daniel PC <daniel-hohmann@outlook.de>
2026-02-15 22:29:01 +0100

    frontend layout version 1

--- frontend-angular19/tlk-frontend19/src/app/app.component.ts
+++ frontend-angular19/tlk-frontend19/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FooterComponent, HeaderComponent, RootComponent} from '@drv-ds/drv-design-system-ng';
-import { HomeComponent } from "./home/home.component";
-import { UploadComponent } from "./upload/upload.component";
+import {FooterComponent} from '@drv-ds/drv-design-system-ng';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RootComponent, HomeComponent, UploadComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
--- frontend-angular19/tlk-frontend19/src/app/app.routes.ts
+++ frontend-angular19/tlk-frontend19/src/app/app.routes.ts
@@ -1,14 +1,29 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { UploadComponent } from "./upload/upload.component";
+﻿import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'upload',
-    component: UploadComponent
-  }
+    loadComponent: () => import('./upload/upload.component').then((m) => m.UploadComponent),
+  },
+  {
+    path: 'summary',
+    loadComponent: () => import('./summary/summary.component').then((m) => m.SummaryComponent),
+  },
+  {
+    path: 'overview',
+    loadComponent: () => import('./overview/overview.component').then((m) => m.OverviewComponent),
+  },
+  {
+    path: 'process',
+    loadComponent: () => import('./process/process.component').then((m) => m.ProcessComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
