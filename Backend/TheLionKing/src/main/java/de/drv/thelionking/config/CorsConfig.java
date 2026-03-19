d413d3d2667933c21e1e319d93ff2d1808f86b6b
Daniel <daniel.hohmann21@gmail.com>
2026-03-19 09:02:48 +0100

    add origin to cors config and remove lines in pom

--- Backend/TheLionKing/src/main/java/de/drv/thelionking/config/CorsConfig.java
+++ Backend/TheLionKing/src/main/java/de/drv/thelionking/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "https://www.drv-tlk.de")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
